000f4281d9bd16b15edfffc03ef4aef4a02305b2
cotowook <cotowook@naver.com>
2023-05-07 08:58:48 +0900

    commit done

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -15,28 +15,38 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.validation.BindingResult;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.shop.dto.ResponseDto;
+
+import com.shop.config.CorsConfig;
+import com.shop.config.SecurityConfig;
+import org.springframework.http.HttpHeaders;
+
 @RequestMapping("/members")
 @Controller
 @RequiredArgsConstructor
-
+// @RestController
+@CrossOrigin
 public class MemberController {
-
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
+    // 리다이렉션 문제 해결을 위한 처리가 필요함
     @GetMapping(value = "/new")
     public String memberForm(Model model){
         model.addAttribute("memberFormDto", new MemberFormDto());
         return "member/memberForm";
     }
 
+    /* 기존의 백엔드 코드
     @PostMapping(value = "/new")
     public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
-
         if(bindingResult.hasErrors()){
             return "member/memberForm";
         }
-
         try {
             Member member = Member.createMember(memberFormDto, passwordEncoder);
             memberService.saveMember(member);
@@ -47,8 +57,19 @@ public class MemberController {
 
         return "redirect:/";
     }
+*/
 
-
+    // 프론트엔드 post 요청을 받은 후 데이터 처리
+    @PostMapping("/new")
+    public ResponseEntity<?> createMember(@RequestBody MemberFormDto memberFormDto) {
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+            return ResponseEntity.ok(new ResponseDto("success", "회원가입이 완료되었습니다."));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(new ResponseDto("error", "회원가입에 실패하였습니다."));
+        }
+    }
 
     @GetMapping(value = "/login")
     public String loginMember(){
