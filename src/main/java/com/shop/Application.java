4ecf8c04f481509d1d9f7d7614fd2cf0efbeb498
cotowook <cotowook@naver.com>
2023-05-07 15:16:15 +0900

    commit done

--- src/main/java/com/shop/Application.java
+++ src/main/java/com/shop/Application.java
@@ -1,8 +1,11 @@
 package com.shop;
 
+import com.shop.config.SecurityConfig;
+import com.shop.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Import;
+@Import({SecurityConfig.class, CorsConfig.class})
 @SpringBootApplication
 public class Application {
 
--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -1,84 +1,33 @@
 package com.shop.controller;
 
 import com.shop.dto.MemberFormDto;
+import com.shop.dto.ResponseDto;
+import com.shop.entity.Member;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.shop.entity.Member;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.validation.BindingResult;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import com.shop.dto.ResponseDto;
-
-import com.shop.config.CorsConfig;
-import com.shop.config.SecurityConfig;
-import org.springframework.http.HttpHeaders;
-
+@RestController
 @RequestMapping("/members")
-@Controller
 @RequiredArgsConstructor
-// @RestController
 @CrossOrigin
 public class MemberController {
     private final MemberService memberService;
     private final PasswordEncoder passwordEncoder;
 
-    // 리다이렉션 문제 해결을 위한 처리가 필요함
-    @GetMapping(value = "/new")
-    public String memberForm(Model model){
-        model.addAttribute("memberFormDto", new MemberFormDto());
-        return "member/memberForm";
-    }
-
-    /* 기존의 백엔드 코드
-    @PostMapping(value = "/new")
-    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
-            return "member/memberForm";
-        }
-        try {
-            Member member = Member.createMember(memberFormDto, passwordEncoder);
-            memberService.saveMember(member);
-        } catch (IllegalStateException e){
-            model.addAttribute("errorMessage", e.getMessage());
-            return "member/memberForm";
-        }
-
-        return "redirect:/";
-    }
-*/
-
-    // 프론트엔드 post 요청을 받은 후 데이터 처리
     @PostMapping("/new")
-    public ResponseEntity<?> createMember(@RequestBody MemberFormDto memberFormDto) {
+    public ResponseDto createMember(@Valid @RequestBody MemberFormDto memberFormDto) {
         try {
             Member member = Member.createMember(memberFormDto, passwordEncoder);
             memberService.saveMember(member);
-            return ResponseEntity.ok(new ResponseDto("success", "회원가입이 완료되었습니다."));
+            return new ResponseDto("success", "회원가입이 완료되었습니다.");
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(new ResponseDto("error", "회원가입에 실패하였습니다."));
+            return new ResponseDto("error", "회원가입에 실패하였습니다.");
         }
     }
 
-    @GetMapping(value = "/login")
-    public String loginMember(){
-        return "/member/memberLoginForm";
-    }
 
-    @GetMapping(value = "/login/error")
-    public String loginError(Model model){
-        model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
-        return "/member/memberLoginForm";
-    }
 }
